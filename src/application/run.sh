4455887e34af2c85b9ca6e9c7f402ae35c9a61c4
shahn <shahn@joyridelabs.de>
2010-05-28 04:32:50 +0800

    run-script checks if hate exists

--- src/application/run.sh
+++ src/application/run.sh
@@ -10,7 +10,12 @@ trap error ERR
 
 echo This is a script that facilitates development and is NOT part of the normal build system.
 
-cabal build 2>&1 | hate 
+if (which hate)
+then 
+    cabal build 2>&1 | hate
+else
+    cabal build
+fi
 
 cd ../..
 src/application/dist/build/nikki/nikki default
