832010811fa0c114335459369a96c4c46c822d9a
shahn <shahn@joyridelabs.de>
2010-10-25 03:58:32 +0800

    actualised graphmod script

--- docs/graphmod.sh
+++ docs/graphmod.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-# script to visualize the module graph. graphmod (> 1.1.3 ???) is needed.
+# Script to visualize the module graph. graphmod (> 1.2.3) is probably needed.
+# Add graphmod options to tweak the graph to your needs.
+
 # graphmod outputs in dot format (so graphviz is probably needed).
 # dot can create other formats than pdf.
 
-graphmod ../src/application/Main.hs -q \
-    -i ../src/application -i ../src/common -i ../src/qtRendering \
-    -R Data -R Graphics -R Physics -r Utils -r Paths \
-    \
-    \
-    -c Top -c Sorts -c Game -c Base -c Editor -c Object\
-    \
+
+graphmod ../src/Main.hs -q -i ../src/ \
+    -R Data -R Graphics -R Physics -r Utils \
     | dot -Tpdf -o mods.pdf
 
 
