3a09be0d412bb326ef24a69f87536b1c1ab1e362
shahn <shahn@joyridelabs.de>
2010-10-30 00:44:26 +0800

    added failing testcase for tile merging

--- src/testsuite/Physics/Chipmunk/StickyEdges/Tests.hs
+++ src/testsuite/Physics/Chipmunk/StickyEdges/Tests.hs
@@ -143,6 +143,11 @@ examples = map (Wrap . map (Wrap . Polygon)) [
         [Vector 4.0 (-4.0),Vector 4.0 1.0,Vector 5.0 1.0,Vector 5.0 (-4.0)],
         [Vector 0.0 (-2.0),Vector 0.0 1.0,Vector 5.0 1.0,Vector 5.0 (-2.0)],
         [Vector (-1.0) (-4.0),Vector (-1.0) 1.0,Vector 4.0 1.0,Vector 4.0 (-4.0)]
+    ],
+    [
+        [Vector (-3.0) 3.0,Vector (-3.0) 5.0,Vector 0.0 5.0,Vector 0.0 3.0],
+        [Vector 0.0 4.0,Vector 0.0 5.0,Vector 2.0 5.0,Vector 2.0 4.0],
+        [Vector (-2.0) 5.0,Vector (-2.0) 10.0,Vector 2.0 10.0,Vector 2.0 5.0]
     ]
   ]
 
