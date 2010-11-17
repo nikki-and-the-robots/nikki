c12bc2accc38e8174ee8691f82b23c739db9a5a1
shahn <shahn@joyridelabs.de>
2010-11-17 08:07:07 +0800

    added comments to Sorts.Nikki.State

--- src/Sorts/Nikki/State.hs
+++ src/Sorts/Nikki/State.hs
@@ -43,8 +43,8 @@ updateState now contacts (True, controlData) nikki = do
             (True, Just (shape, contactAngle)) -> State
                 (JumpImpulse now shape contactAngle velocity_ buttonDirection)
                 (jumpImpulseDirection contactAngle)
-            -- nikki touches something
             (False, Just _) ->
+            -- nikki touches something
                 case grips nikkiPos contacts of
                     -- nikki grabs something
                     Just HLeft | rightPushed -> State EndGripImpulse HLeft
@@ -55,30 +55,38 @@ updateState now contacts (True, controlData) nikki = do
                         if hasLegsCollisions then
                         -- something touches the feet
                             if standsOnFeet then
+                            -- nikki stands on feet (the angle is not too steep)
                                 if nothingHeld then
                                     State Wait newDirection
                                 else
                                     State Walk newDirection
-                              else
+                            else
+                            -- the angle is too steep: nikki slides into grip mode (hopefully)
                                 State
                                     (SlideToGrip (jumpInformation' velocity_))
                                     newDirection
-                          else
-                            State (WallSlide (jumpInformation' velocity_)
+                        else
+                        -- something touches the head
+                            State
+                                (WallSlide (jumpInformation' velocity_)
                                     (map snd contactNormals)
                                     (clouds nikkiPos newDirection))
                                 newDirection
             (_, Nothing) ->
+            -- nikki touches nothing, is airborne
                 State (Airborne (jumpInformation' velocity_)) newDirection
 
+    -- if nikki should jump. Jump button is pushed and nikki is not in slideToGrip mode.
     willJump :: Bool
     willJump = aPushed && not (isSlideToGrip (state nikki))
 
+    -- nikki's new horizontal direction
     newDirection :: HorizontalDirection
     newDirection = case state nikki of
         State Grip direction -> swapHorizontalDirection direction
         _ -> fromMaybe oldDirection buttonDirection
-    -- returns if nikki grabs something
+
+    -- returns if nikki grabs something (and if yes, which direction)
     grips :: CM.Position -> Contacts -> Maybe HorizontalDirection
     grips nikkiPos =
         nikkiCollisions >>>
@@ -90,35 +98,47 @@ updateState now contacts (True, controlData) nikki = do
         inner gripCollisions = Just $
             if any ((NikkiLeftPawCT ==) . nikkiCollisionType) gripCollisions
             then HLeft else HRight
+    -- if a given collision is with nikki's head
     isHeadCollision (NikkiCollision _ normal NikkiHeadCT) = True
     isHeadCollision (NikkiCollision _ normal NikkiLeftPawCT) = True
     isHeadCollision _ = False
+    -- if a given head collision should be treated as nikki grabbing something
     isGripCollision c =
         abs (foldAngle $ toUpAngle $ nikkiCollisionNormal c) < gripAngleLimit
 
-
+    -- if nikki should be considered standing on feet.
     standsOnFeet = hasStandingFeetCollisions ||
         hasCombinedStandingFeetNormal
 
+    -- if nikki has collisions with the legs
     hasLegsCollisions = not (null legsCollisions)
+    -- list of leg collisions
     legsCollisions = filter isLegsCollision $ nikkiCollisions contacts
+    -- if a given collision is with nikki's legs
     isLegsCollision (NikkiCollision _ _ NikkiLegsCT) = True
     isLegsCollision _ = False
 
+    -- if one of the leg collisions has the right angle to lead to nikki standing on feet
     hasStandingFeetCollisions =
         any (nikkiCollisionNormal >>> isStandingFeetNormal) legsCollisions
+    -- if a given collision normal should lead to nikki standing on feet
     isStandingFeetNormal normal =
         isStandingFeetAngle $ foldAngle $ toUpAngle normal
+    -- if a given collision angle should lead to nikki standing on feet
     isStandingFeetAngle angle =
         abs angle < (deg2rad 90 - footToHeadAngle + deg2rad 1)
                                                     -- angle epsilon
 
-    -- if two (or more) collisions result in a contactAngle that allows jumping
+    -- If the collisions together result in a contactAngle that allows jumping.
+    -- (There might be two (or more) angles, that -- if considered individually -- would
+    -- lead to nikki not standing on feet (and sliding to grip mode), but together
+    -- should allow jumping, cause they have differing signs.)
     hasCombinedStandingFeetNormal :: Bool
     hasCombinedStandingFeetNormal = case mContactAngle of
         Nothing -> False
         Just (_, contactAngle) -> isStandingFeetAngle contactAngle
 
+    -- button events
     aPushed = Press AButton `elem` pressed controlData
     aHeld = AButton `member` held controlData
     rightPushed = Press RightButton `elem` pressed controlData
@@ -127,39 +147,40 @@ updateState now contacts (True, controlData) nikki = do
     leftHeld = LeftButton `member` held controlData
     nothingHeld = not (rightHeld `xor` leftHeld)
 
+    -- nikkis previous horizontal direction
     oldDirection :: HorizontalDirection
     oldDirection = direction $ state nikki
+    -- the direction indicated by the buttons (if any)
     buttonDirection :: Maybe HorizontalDirection
     buttonDirection =
         if nothingHeld then Nothing else
         if leftHeld then Just HLeft else
         Just HRight
-    verticalDirection velocity_ = if vectorY velocity_ <= 0 then VUp else VDown
 
+    -- the contact angle that should be used for jumping (if there are collisions)
     mContactAngle :: Maybe (Shape, Angle)
     mContactAngle = jumpAngle contactNormals
+    -- all collision normals (and their corresponding shapes)
     contactNormals :: [(Shape, Angle)]
     contactNormals = getContactNormals contacts
 
+    -- while nikki is in the air, this describes the state
     jumpInformation' velocity_ =
         JumpInformation jumpStartTime_ velocity_ buttonDirection
 
+    -- when the jump button is held, this saves the time of the jump's start
     jumpStartTime_ :: Maybe Seconds
     jumpStartTime_ = case action $ state nikki of
         JumpImpulse t _ _ _ _ -> Just t
         x | aHeld -> jumpStartTime =<< getJumpInformation x
         x -> Nothing
 
-    angleDirection :: Angle -> Maybe HorizontalDirection
-    angleDirection angle =
-        if abs angle > deg2rad 10 then
-            Just $ if angle > 0 then HRight else HLeft
-          else
-            Nothing
-
+    -- | direction when starting a jump
     jumpImpulseDirection angle = fromMaybe oldDirection
         (buttonDirection <|> angleDirection angle)
 
+    -- | create nikki's dust
+    clouds :: Vector -> HorizontalDirection -> [Cloud]
     clouds (Vector x y) direction = case action $ state nikki of
         WallSlide _ _ (a : r) ->
             if now - creationTime a > cloudCreationTime then
@@ -174,6 +195,12 @@ updateState now contacts (True, controlData) nikki = do
         newCloud HRight = Cloud now (Position x y +~ Position (fromUber (13 / 2)) (fromUber (24 / 2)) +~ cloudRenderCorrection)
         cloudRenderCorrection = Position (- fromUber (5 / 2)) (- fromUber (5 / 2))
 
+-- | returns the horizontal direction of a given angle
+angleDirection :: Angle -> Maybe HorizontalDirection
+angleDirection angle =
+    if abs angle > deg2rad 10
+    then Just $ if angle > 0 then HRight else HLeft
+    else Nothing
 
 -- | updates the possible jumping angle from the contacts
 getContactNormals :: Contacts -> [(Shape, Angle)]
@@ -194,6 +221,7 @@ jumpAngle angles =
               else
                 Just a
 
+-- updates the start time of nikki if applicable
 updateStartTime :: Seconds -> State -> Nikki -> Nikki
 updateStartTime now oldState nikki =
     if sameState oldState (state nikki) then
@@ -201,6 +229,8 @@ updateStartTime now oldState nikki =
       else
         nikki{startTime = now}
 
+-- returns if two given state have the same type of action
+sameState :: State -> State -> Bool
 sameState a b =
     toActionNumber (action a) == toActionNumber (action b)
     && direction a == direction b
