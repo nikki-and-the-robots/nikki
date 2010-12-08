5d0e7cce649c917beeb8594704c16feb53f1e411
shahn <shahn@cs.tu-berlin.de>
2010-12-08 23:17:51 +0800

    added some documentation and argument passing to windowsCompile.sh

--- src/windowsCompile.sh
+++ src/windowsCompile.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-# to abort in case of errors
+# This script attempts to build the game on windows.
+# It invokes cabal configure with a lot of options to convince it
+# to use the build toolchain in the $PATH and not the one shipped with ghc.
+# (Which should be the toolchain shipped with Qt in our case.)
+#
+# cabal dependencies must be installed using the same compiler, e.g.:
+# cabal install --with-ar=ar --with-gcc=gcc --with-ld=ld --ghc-options=" -pgmc gcc -pgml gcc -pgma gcc -pgmwindres windres -pgmP \"gcc -E -undef -traditional\"" package
 
+echo This script will pass any command line options to "cabal configure".
+
+# to abort in case of errors
 function error {
 echo aborted due to an error
 exit 1
 }
 trap error ERR
-
-# install cabal deps with
-# cabal install --with-ar=ar --with-gcc=gcc --with-ld=ld --ghc-options=" -pgmc gcc -pgml gcc -pgma gcc -pgmwindres windres -pgmP \"gcc -E -undef -traditional\""
 
 # building c++-part (qt-bindings)
 cd cpp
@@ -19,5 +25,5 @@ cmake -G "MSYS Makefiles" ..
 make
 cd ../..
 
-cabal configure --with-ar=ar --with-gcc=gcc --with-ld=ld --ghc-options=" -pgmc gcc -pgml gcc -pgma gcc -pgmwindres windres -pgmP \"gcc -E -undef -traditional\""
+cabal configure --with-ar=ar --with-gcc=gcc --with-ld=ld --ghc-options=" -pgmc gcc -pgml gcc -pgma gcc -pgmwindres windres -pgmP \"gcc -E -undef -traditional\"" $@
 cabal build
