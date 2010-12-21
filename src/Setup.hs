da21e4d280fc549780d50fb85c45439605b352c5
shahn <shahn@joyridelabs.de>
2010-12-22 01:39:19 +0800

    better Setup script for mac deployment. Copies the qt_menu-nib-dir to the app bundle.

--- src/Setup.hs
+++ src/Setup.hs
@@ -1,52 +1,110 @@
 
+module Main where
+
 
 import Data.List
+import Data.Char
 
 import Control.Applicative
 import Control.Monad
+import Control.Exception
 
 import System.Directory
+import System.Posix.Directory (changeWorkingDirectory)
 import System.FilePath
 import System.Info
 import System.Process
 
+import Distribution.PackageDescription
 import Distribution.Simple
+import Distribution.Simple.Setup
+import Distribution.Simple.LocalBuildInfo
 import Distribution.MacOSX
 
 
 main =
   case os of
-    "darwin" -> defaultMainWithHooks =<< macDeploymentHooks
+    "darwin" -> macMain
     _ -> defaultMain
 
-macDeploymentHooks :: IO UserHooks
-macDeploymentHooks = do
-    -- this is a hack to convince cabal-macosx not to flatten the resource files
-    e <- doesDirectoryExist "resources"
-    when (not e) $ do
-        system "ln -s ../data resources"
-        return ()
-    resourceFiles <- getFilesRecursive "resources"
-    return $ simpleUserHooks{postBuild = appBundleBuildHook [(macApp resourceFiles)]}
+-- * mac deployment
+
+macMain :: IO ()
+macMain = defaultMainWithHooks simpleUserHooks{postBuild = macPostBuild}
+
+macResourcesDir = "resources"
+
+macPostBuild :: Args -> BuildFlags -> PackageDescription -> LocalBuildInfo -> IO ()
+macPostBuild args buildFlags packageDescription localBuildInfo =
+    withTemporaryDirectoryCopy "../data" macResourcesDir $ do
+        qtLibDir <- trim <$> readProcess "qmake" ["-query", "QT_INSTALL_LIBS"] ""
+        let qtNibDir = qtLibDir </> macResourcesDir </> "qt_menu.nib"
+        copyDirectory qtNibDir (macResourcesDir </> "qt_menu.nib")
+        resources <- map (macResourcesDir </>) <$> getFilesRecursive macResourcesDir
+        appBundleBuildHook [macApp resources] args buildFlags packageDescription localBuildInfo
 
 -- | deployment on a mac
 macApp :: [FilePath] -> MacApp
 macApp resourceFiles = MacApp {
     appName = "nikki",
-    appIcon = Just "../data/png/icon-128.png",
+    appIcon = Just (macResourcesDir </> "png/icon-128.png"),
     appPlist = Nothing,
     resources = resourceFiles,
     otherBins = [],
     appDeps = ChaseWithDefaults
   }
 
--- | returns all (unhidden) files in a directory recursively,
--- excluding all directories
+
+-- * utils
+
+-- | returns all (unhidden) files in a directory recursively
 getFilesRecursive :: FilePath -> IO [FilePath]
-getFilesRecursive dir = do
-    content <- filter (not . ("." `isPrefixOf`)) <$> getDirectoryContents dir
-    let here = map (dir </>) content
-    files <- filterM (\ p -> not <$> doesDirectoryExist p) here
-    directories <- filterM doesDirectoryExist here
-    recursive <- mapM getFilesRecursive directories
-    return $ sort (files ++ concat recursive)
+getFilesRecursive dir =
+    withWorkingDirectory dir $ do
+        map normalise <$> inner "."
+  where
+    inner dir = do
+        content <- map (dir </>) <$> filter (not . ("." `isPrefixOf`)) <$> getDirectoryContents dir
+        (directories, files) <- partitionM doesDirectoryExist content
+        recursive <- mapM inner $ directories
+        return $ sort (files ++ concat recursive)
+
+-- | change the workingdirectory during the execution of the given action
+withWorkingDirectory :: FilePath -> IO a -> IO a
+withWorkingDirectory dir action = do
+    wd <- getCurrentDirectory
+    (changeWorkingDirectory dir >> action) `finally` changeWorkingDirectory wd
+
+-- | copy a whole directory recursively
+-- excluding hidden files
+copyDirectory :: FilePath -> FilePath -> IO ()
+copyDirectory src dst = do
+    allFiles <- getFilesRecursive src
+    forM_ allFiles copy
+  where
+    copy file = do
+        createDirectoryIfMissing True (takeDirectory (dst </> file))
+        copyFile (src </> file) (dst </> file)
+
+-- | copy a directory, perform a given action, then delete the copy
+withTemporaryDirectoryCopy :: FilePath -> FilePath -> IO a -> IO a
+withTemporaryDirectoryCopy original copy action = do
+    eDir <- doesDirectoryExist copy
+    eFile <- doesFileExist copy
+    when (eDir || eFile) $
+        fail ("directory (or file?) already exists: " ++ copy)
+    (copyDirectory original copy >> action) ` finally` removeDirectoryRecursive copy
+
+partitionM :: (a -> IO Bool) -> [a] -> IO ([a], [a])
+partitionM p (a : r) = do
+    condition <- p a
+    (yes, no) <- partitionM p r
+    return $ if condition then
+        (a : yes, no)
+      else
+        (yes, a : no)
+partitionM _ [] = return ([], [])
+
+-- | remove surrounding whitespaces
+trim :: String -> String
+trim = dropWhile isSpace . reverse . dropWhile isSpace . reverse
