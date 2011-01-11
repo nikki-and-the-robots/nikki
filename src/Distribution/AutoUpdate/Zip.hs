5f78edfe68a84101731d240523b7772277a441cc
shahn <shahn@joyridelabs.de>
2011-01-12 03:57:57 +0800

    making sure the zipfile is closed after reading

--- src/Distribution/AutoUpdate/Zip.hs
+++ src/Distribution/AutoUpdate/Zip.hs
@@ -8,7 +8,9 @@ import qualified Data.ByteString.Lazy as BS
 import Codec.Archive.Zip
 
 import Control.Monad
+import Control.Exception
 
+import System.IO
 import System.FilePath
 import System.Directory
 
@@ -19,19 +21,31 @@ import Utils
 unzipArchive :: (String -> IO ()) -> FilePath -> FilePath -> IO ()
 unzipArchive logCommand zipFile directory = do
     logCommand ("unzipping " ++ zipFile)
-    archive <- toArchive <$> BS.readFile zipFile
-    forM_ (zEntries archive) $ \ entry -> do
-        -- modifying the path of the entry to unpack in a given folder.
-        let unpackPath = directory </> normalise (eRelativePath entry)
-        print unpackPath
-        if isZippedDirectory unpackPath then
-            createDirectoryIfMissing True unpackPath
-          else
-            writeEntry [] entry{eRelativePath = unpackPath}
+    withBinaryFile zipFile $ \ content -> do
+        let archive = toArchive content
+        forM_ (zEntries archive) $ \ entry -> do
+            -- modifying the path of the entry to unpack in a given folder.
+            let unpackPath = directory </> normalise (eRelativePath entry)
+            print unpackPath
+            if isZippedDirectory unpackPath then
+                createDirectoryIfMissing True unpackPath
+              else
+                writeEntry [] entry{eRelativePath = unpackPath}
   where
     isZippedDirectory :: FilePath -> Bool
     isZippedDirectory "" = True
     isZippedDirectory f = last f `elem` pathSeparators
+
+    -- Opens a file and performs the given action on the content.
+    -- Makes sure the file handle will be closed.
+    withBinaryFile :: FilePath -> (BS.ByteString -> IO a) -> IO a
+    withBinaryFile file action = do
+        bracket open close $ \ handle -> do
+            action =<< BS.hGetContents handle
+      where
+        open = openBinaryFile file ReadMode
+        close h = hClose h >> putStrLn "closed!!!"
+
 
 -- | zips a given folder recursively into the given zipFile
 zipArchive :: (String -> IO ()) -> FilePath -> FilePath -> IO ()
