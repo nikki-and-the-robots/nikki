5a090640c4a187f76c3ed71b5bd9496617524e2a
shahn <shahn@joyridelabs.de>
2011-01-13 22:18:05 +0800

    silent restarter

--- src/Distribution/AutoUpdate/Restarter.hs
+++ src/Distribution/AutoUpdate/Restarter.hs
@@ -13,14 +13,18 @@ import System.Exit
 
 import Distribution.AutoUpdate.Paths
 
+import Base.Configuration
+
 
 main = do
+    loadConfiguration
     executable <- findCoreExecutable
     args <- getArgs
     loop 5 $ rawSystem executable args
   where
     loop 0 action = error "restarted 5 times: aborting"
     loop n action = do
+        logInfo "starting core..."
         exitCode <- action
         case exitCode of
             ExitFailure 143 -> do
