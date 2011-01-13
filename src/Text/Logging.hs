fef252f68ffe8fa620b068b4f338e510f6b97799
shahn <shahn@joyridelabs.de>
2011-01-13 21:12:09 +0800

    added missing file

---
+++ src/Text/Logging.hs
@@ -0,0 +1,33 @@
+
+-- | Module for logging:
+-- Decides how to log.
+-- Saves a global logging operation.
+
+module Text.Logging (logInfo, setLogCommand, printInfo) where
+
+
+import Control.Monad.IO.Class
+import Control.Concurrent.MVar
+
+import System.IO.Unsafe
+
+-- | log something (info level)
+logInfo :: MonadIO m => String -> m ()
+logInfo msg = liftIO $ do
+    logCommand <- readMVar logCommandRef
+    logCommand msg
+
+-- | like print, but uses the logging mechanisms of this module
+-- (instead of stdout)
+printInfo :: (MonadIO m, Show s) => s -> m ()
+printInfo x = logInfo (show x)
+
+{-# NOINLINE logCommandRef #-}
+logCommandRef :: MVar (String -> IO ())
+logCommandRef = unsafePerformIO $ newMVar putStrLn
+
+-- | sets a new log command.
+setLogCommand :: MonadIO m => (String -> IO ()) -> m ()
+setLogCommand logCommand = liftIO $ do
+    swapMVar logCommandRef logCommand
+    return ()
