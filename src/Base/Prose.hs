3fe28f98c2cadfaca350e5894324a2e5ae839d66
shahn <shahn@joyridelabs.de>
2011-01-22 09:38:03 +0800

    reading help files in binary mode

--- src/Base/Prose.hs
+++ src/Base/Prose.hs
@@ -35,4 +35,14 @@ p = Prose . encode
 -- (Needs to be separated from p, because it has to return multiple lines.)
 pFile :: FilePath -> IO [Prose]
 pFile file =
-    map p <$> lines <$> readFile file
+    map Prose <$> Base.Prose.lines <$> BS.readFile file
+
+lines :: BS.ByteString -> [BS.ByteString]
+lines x =
+    inner BS.empty x
+  where
+    inner :: BS.ByteString -> BS.ByteString -> [BS.ByteString]
+    inner akk x = case BS.uncons x of
+        Nothing -> [akk]
+        Just (a, r) | a == 10 -> akk : inner BS.empty (BS.tail x)
+        Just (a, r) -> inner (akk `BS.snoc` a) r
