70d93756e9145a42bf175268ac3a58f1680ab67f
shahn <shahn@joyridelabs.de>
2011-02-01 19:45:03 +0800

    removed dependencies from restarter

--- src/Distribution/AutoUpdate/Restarter.hs
+++ src/Distribution/AutoUpdate/Restarter.hs
@@ -3,31 +3,33 @@
 module Main where
 
 
-import Text.Logging
+import Prelude hiding (log)
 
 import System.Environment
 import System.Process
 import System.Exit
+import System.Info
 
 import Distribution.AutoUpdate.Paths
 
-import Base.Configuration
-
 
 main = do
-    loadConfiguration
     executable <- findCoreExecutable
     args <- getArgs
     loop 5 $ rawSystem executable args
   where
     loop 0 action = error "restarted 5 times: aborting"
     loop n action = do
-        logInfo "starting core..."
+        log "starting core..."
         exitCode <- action
         case exitCode of
             ExitFailure 143 -> do
-                logInfo (show exitCode ++ " -> restarting")
+                log (show exitCode ++ " -> restarting")
                 loop (n - 1) action
             _ -> do
-                logInfo (show exitCode ++ " -> not restarting")
+                log (show exitCode ++ " -> not restarting")
                 exitWith exitCode
+
+log = case System.Info.os of
+    "mingw32" -> appendFile "nikkiLog" . (++ "\n")
+    _ -> putStrLn
