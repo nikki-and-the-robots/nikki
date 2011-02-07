eb6b7ec978a2da915bd1ab71fdb8068847113302
shahn <shahn@joyridelabs.de>
2011-02-07 23:58:55 +0800

    removed logging output

--- src/System/Environment/FindBin/Fixed.hs
+++ src/System/Environment/FindBin/Fixed.hs
@@ -5,8 +5,6 @@
 module System.Environment.FindBin.Fixed where
 
 
-import Text.Logging
-
 import System.Info
 import System.FilePath
 import System.Directory
@@ -25,15 +23,13 @@ import Utils
 -- | this works around the issue of hackage's FindBin, but it doesn't work
 -- when the program is searched executed via $PATH.
 getProgPath :: IO FilePath
-getProgPath = log =<< case System.Info.os of
+getProgPath = case System.Info.os of
     os | os `elem` ("linux" : "darwin" : []) -> do
         dir <- getCurrentDirectory
         prog <- getProgName
         (fullProgName : _) <- wrap getFullProgArgv
         takeDirectory <$> canonicalizePath (dir </> fullProgName)
     _ -> System.Environment.FindBin.getProgPath
-  where
-    log x = logInfo ("getProgPath: " ++ x) >> return x
 
 wrap :: (Ptr CInt -> Ptr (Ptr CString) -> IO ()) -> IO [String]
 wrap action = alloca $ \ argcPtr -> alloca $ \ argvArrayPtr -> do
