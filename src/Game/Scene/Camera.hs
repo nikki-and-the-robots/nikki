e5020f9f4144a630a4ea5f7de7add50afdfba662
shahn <shahn@joyridelabs.de>
2011-02-10 22:58:54 +0800

    changed camera behaviour

--- src/Game/Scene/Camera.hs
+++ src/Game/Scene/Camera.hs
@@ -1,3 +1,4 @@
+{-# language NamedFieldPuns #-}
 
 -- | module to compute the camera position
 
@@ -9,6 +10,7 @@ module Game.Scene.Camera (
 
 
 import Data.Abelian
+import Data.Indexable
 
 import Control.Monad.State
 
@@ -39,36 +41,57 @@ initialCameraState :: CameraState
 initialCameraState = CS (- 1) zero
 
 getCameraPosition :: Qt.Ptr Qt.QPainter -> Scene Object_ -> StateT CameraState IO Position
-getCameraPosition ptr scene = do
+getCameraPosition ptr scene =
+    case mode scene of
+        NikkiMode{nikki} -> getCameraPositionMaybeCentered ptr scene nikki
+        TerminalMode{} -> do
+            -- leave the camera on the old controlled object
+            CS oldIndex oldPosition <- get
+            getPositionForIndex ptr scene oldPosition oldIndex
+        RobotMode{robot} -> getCameraPositionMaybeCentered ptr scene robot
+        LevelFinished{} -> do
+            -- level is finished, not in game mode anymore
+            CS index oldPosition <- get
+            let controlledObject = getMainlayerObject scene index
+            if isNikki $ sort_ controlledObject then
+                -- camera follows Nikki
+                getPositionForIndex ptr scene oldPosition index
+              else
+                -- camera followed robot
+                return oldPosition
+
+-- | Returns the camera position following the given object.
+-- Centers the camera on the object, if the object changed since last time.
+getCameraPositionMaybeCentered :: Qt.Ptr Qt.QPainter -> Scene Object_ -> Index
+    -> StateT CameraState IO Position
+getCameraPositionMaybeCentered ptr scene index = do
     CS oldIndex oldPosition <- get
-    if isTerminalMode $ mode scene then do
-        -- don't move camera in terminal mode
-        -- (and reset the controlled index (to center camera anew))
-        put $ CS (- 1) oldPosition
-        return oldPosition
-      else case getControlledIndex scene of
-        -- level is finished, not in game mode anymore
-        Nothing -> return oldPosition
-        -- update via the controlled object
-        Just controlledIndex -> do
-            let controlledObject = getMainlayerObject scene controlledIndex
-            controlledPosition <- io $ getPosition $ getControlledChipmunk scene controlledObject
-            if controlledIndex /= oldIndex then do
-                -- controlled objects has changed -> center object on screen
-                put $ CS controlledIndex controlledPosition
-                return controlledPosition
-              else do
-                -- same object -> bounding box behaviour                
-                windowSize <- fmap fromIntegral <$> io (Qt.sizeQPainter ptr)
-                let limit = min maximumLimit (Qt.height windowSize * partialLimit / 2)
-                    -- vertical distance from the controlled object to the camera's old position
-                    controlledToCamera = vectorY oldPosition - vectorY controlledPosition
-                    y = if controlledToCamera < (- limit) then 
-                            vectorY controlledPosition - limit
-                        else if controlledToCamera > limit then
-                            vectorY controlledPosition + limit
-                        else
-                            vectorY oldPosition
-                    newPosition = Vector (vectorX controlledPosition) y
-                put $ CS controlledIndex newPosition
-                return newPosition
+    if index == oldIndex then
+        -- same object -> bounding box behaviour
+        getPositionForIndex ptr scene oldPosition index
+      else do
+        -- controlled objects has changed -> center object on screen
+        let controlledObject = getMainlayerObject scene index
+        controlledPosition <- io $ getPosition $ getControlledChipmunk scene controlledObject
+        put $ CS index controlledPosition
+        return controlledPosition
+
+-- | Returns the camera position that follows the given (controlled) object.
+-- Implements the bounding box.
+getPositionForIndex :: Qt.Ptr Qt.QPainter -> Scene Object_ -> Vector -> Index -> StateT CameraState IO Position
+getPositionForIndex ptr scene oldPosition index = do
+    let controlledObject = getMainlayerObject scene index
+    controlledPosition <- io $ getPosition $ getControlledChipmunk scene controlledObject
+    windowSize <- fmap fromIntegral <$> io (Qt.sizeQPainter ptr)
+    let limit = min maximumLimit (Qt.height windowSize * partialLimit / 2)
+        -- vertical distance from the controlled object to the camera's old position
+        controlledToCamera = vectorY oldPosition - vectorY controlledPosition
+        y = if controlledToCamera < (- limit) then 
+                vectorY controlledPosition - limit
+            else if controlledToCamera > limit then
+                vectorY controlledPosition + limit
+            else
+                vectorY oldPosition
+        newPosition = Vector (vectorX controlledPosition) y
+    put $ CS index newPosition
+    return newPosition
