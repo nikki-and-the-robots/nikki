f1121e066527e1dd380f23ee20db958bb3bd8937
shahn <shahn@joyridelabs.de>
2011-03-20 01:58:54 +0800

    tidied up FPS system

--- src/Base/FPSState.hs
+++ src/Base/FPSState.hs
@@ -6,10 +6,8 @@ module Base.FPSState (
     FpsState,
     initialFPSState,
     tickFPS,
-    terminateFpsState,
     initialFPSRef,
     tickFPSRef,
-    terminateFPSRef,
   ) where
 
 
@@ -46,44 +44,38 @@ data FpsState
         counter :: !Int,
         averageSpan :: !(Maybe Double),
         oldTime :: Maybe Double,
-        logHandle :: Maybe Handle,
         displayValue :: IORef Prose
     }
     | NotActivated
 
-
-logFile = "fps.dat"
 
 -- creates the initial FpsState
 initialFPSState :: M FpsState
 initialFPSState = do
     graphicsProfiling_ <- gets graphics_profiling
     if graphicsProfiling_ then do
---         logHandle <- openFile logFile WriteMode
---         return $ FpsState 0 Nothing Nothing logHandle
         displayValue <- io $ newIORef (pVerbatim "")
-        return $ FpsState 0 Nothing Nothing Nothing displayValue
+        return $ FpsState 0 Nothing Nothing displayValue
       else
         return NotActivated
 
 -- does the actual work. Must be called for every frame
 tickFPS :: Font -> Ptr QPainter -> FpsState -> IO FpsState
-tickFPS font ptr (FpsState counter avg Nothing logHandle displayValue) = do
+tickFPS font ptr (FpsState counter avg Nothing displayValue) = do
     -- first time: QTime has to be constructed
     now <- getNow
-    return $ FpsState counter avg (Just now) logHandle displayValue
-tickFPS font ptr (FpsState counter avg (Just oldTime) logHandle displayValue) = do
+    return $ FpsState counter avg (Just now) displayValue
+tickFPS font ptr (FpsState counter avg (Just oldTime) displayValue) = do
         now <- getNow
         let elapsed = now - oldTime
-        log elapsed
-        let avg' = calcAvg counter avg elapsed
-        r <- handle (FpsState (counter + 1) (Just avg') (Just now) logHandle displayValue)
+            avg' = calcAvg counter avg elapsed
+        r <- handle (FpsState (counter + 1) (Just avg') (Just now) displayValue)
         io (renderFPS font ptr =<< readIORef displayValue)
         return r
   where
-    handle x@(FpsState 10 (Just avg) qtime lf dv) = do
+    handle x@(FpsState 10 (Just avg) qtime dv) = do
         writeIORef dv (pVerbatim $ printf "FPS: %3.1f" (1 / avg))
-        return $ FpsState 0 Nothing qtime lf dv
+        return $ FpsState 0 Nothing qtime dv
     handle x = return x
 
     calcAvg :: Int -> Maybe Double -> Double -> Double
@@ -92,8 +84,6 @@ tickFPS font ptr (FpsState counter avg (Just oldTime) logHandle displayValue) = 
         (lenF * avg + newValue) / (lenF + 1)
       where lenF = fromIntegral len
 
-    log elapsed = whenMaybe logHandle $ \ h -> 
-                    hPutStrLn h (show elapsed)
 -- no FPS activated (NotActivated)
 tickFPS _ _ x = return x
 
@@ -103,14 +93,6 @@ renderFPS font ptr fps = do
     size <- fmap fromIntegral <$> sizeQPainter ptr
     translate ptr (Position (fromUber 2) (height size - fontHeight font))
     void $ renderLineSimple font Nothing white fps ptr
-
-terminateFpsState :: FpsState -> IO ()
-terminateFpsState FpsState{logHandle = Just h} = do
-    hClose h
-    writeDistribution logFile "profiling/distribution.dat"
--- NotActivated
-terminateFpsState _ = return ()
-
 
 writeDistribution :: FilePath -> FilePath -> IO ()
 writeDistribution srcFile destFile = do
@@ -138,7 +120,3 @@ initialFPSRef =
 tickFPSRef :: Font -> Ptr QPainter -> FPSRef -> IO ()
 tickFPSRef font ptr (FPSRef ref) =
     readIORef ref >>= tickFPS font ptr >>= writeIORef ref
-
-terminateFPSRef :: FPSRef -> IO ()
-terminateFPSRef (FPSRef ref)  =
-    readIORef ref >>= terminateFpsState
