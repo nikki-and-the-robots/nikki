32f29b9d727b27b29f5d7fffac4ed5afe69063cc
shahn <shahn@joyridelabs.de>
2011-03-22 02:48:06 +0800

    loading and saving community levels from .nikki-free-levels, even when run in place.

--- src/Base/LevelLoading.hs
+++ src/Base/LevelLoading.hs
@@ -5,17 +5,13 @@ module Base.LevelLoading (
   ) where
 
 
-import Control.Monad
-
 import System.FilePath
 import System.Directory
 
 import Utils
 
 import Base.Types
-import Base.Monad
 import Base.Paths
-import Base.Configuration
 
 
 -- | Returns all files that can be played.
@@ -23,28 +19,18 @@ import Base.Configuration
 lookupLevels :: RM [FilePath]
 lookupLevels = do
     standardLevels <- getDataFiles "standard_levels" (Just ".nl")
-    run_in_place_ <- asks run_in_place
-    if (not run_in_place_) then do
-        levelDirectory <- getFreeLevelsDirectory
-        userLevels <- map (levelDirectory </>) <$> io (getFiles levelDirectory (Just "nl"))
-        return (standardLevels ++ userLevels)
-      else
-        return standardLevels
+    levelDirectory <- getFreeLevelsDirectory
+    userLevels <- map (levelDirectory </>) <$> io (getFiles levelDirectory (Just "nl"))
+    return (standardLevels ++ userLevels)
 
 -- | Return the directory where levels are (and should be) saved.
--- If --level-path is set, this will be used.
--- Otherwise a standard directory is returned (using getAppUserDataDirectory "nikki-free-levels").
--- If the standard directory does not exist, it will be initialised
--- with a set of standard levels.
+-- A standard directory is returned (using getAppUserDataDirectory "nikki-free-levels").
+-- If the standard directory does not exist, it will be created.
 getFreeLevelsDirectory :: RM FilePath
 getFreeLevelsDirectory = do
-    run_in_place_ <- asks run_in_place
-    if run_in_place_ then
-        getDataFileName "standard_levels"
-      else do
-        freeLevelsDirectory <- io $ getAppUserDataDirectory "nikki-free-levels"
-        exists <- io $ doesDirectoryExist freeLevelsDirectory
-        when (not exists) $ do
-            -- create directory
-            io $ createDirectory freeLevelsDirectory
-        return freeLevelsDirectory
+    freeLevelsDirectory <- io $ getAppUserDataDirectory "nikki-free-levels"
+    exists <- io $ doesDirectoryExist freeLevelsDirectory
+    when (not exists) $ do
+        -- create directory
+        io $ createDirectory freeLevelsDirectory
+    return freeLevelsDirectory
