6e421952b11914e0e596c797b6eb3e5c2044cd13
shahn <shahn@joyridelabs.de>
2011-04-13 00:42:01 +0800

    minor version bump

--- src/Version.hs
+++ src/Version.hs
@@ -22,7 +22,7 @@ import Utils
 nikkiVersion :: Version
 nikkiVersion = Version version tags
   where
-    version = 0 : 2 : 21 : []
+    version = 0 : 2 : 22 : []
     tags = []
 
 parseVersion :: String -> Either String Version
