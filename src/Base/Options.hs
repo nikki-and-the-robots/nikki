0ad621cace6019db66c7bd6466448ac7c4e71d9a
shahn <shahn@joyridelabs.de>
2011-05-03 01:35:36 +0800

    show if fullscreen mode is on or off in options

--- src/Base/Options.hs
+++ src/Base/Options.hs
@@ -4,17 +4,31 @@ module Base.Options (
   ) where
 
 
+import Utils
+
 import Base.Types
+import Base.Configuration
 import Base.Monad
 import Base.Prose
+import Base.Application
 
 import Base.Renderable.Menu
 
 
 -- | options that are used in the main menu and in the game (and possibly the editor?)
 generalOptions :: Application_ s -> Int -> Parent -> AppState
-generalOptions app ps parent = menuAppState app (Just $ p "options") (Just parent)
-   ((p "fullscreen", \ ps -> NoGUIAppState (swapFullScreen app >> return (this ps))) :
-   []) ps
+generalOptions app ps parent = NoGUIAppState $ do
+    fullScreenMenuItem <- mkFullScreen app this
+    let menuItems =
+            fullScreenMenuItem :
+            []
+    return $ menuAppState app (Just $ p "options") (Just parent) menuItems ps
   where
     this ps = generalOptions app ps parent
+
+mkFullScreen :: Application_ s -> (Int -> AppState) -> M (Prose, Int -> AppState)
+mkFullScreen app parent = do
+    on <- gets fullscreen
+    let switchText = if on then p "on" else p "off"
+        text = p "fullscreen" +> pVerbatim ": " +> switchText
+    return (text, \ ps -> NoGUIAppState (swapFullScreen app >> return (parent ps)))
