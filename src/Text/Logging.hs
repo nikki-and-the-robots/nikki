1a80796337658200e0bb8f0c676dac2bd08ec30a
shahn <shahn@joyridelabs.de>
2011-07-26 01:35:25 +0800

    printing only messaged more important or equal to (Info :: LogLevel)

--- src/Text/Logging.hs
+++ src/Text/Logging.hs
@@ -4,6 +4,8 @@
 module Text.Logging (LogLevel(..), logg) where
 
 
+import Control.Monad
+
 import System.Info
 import System.FilePath
 import System.Environment
@@ -12,12 +14,15 @@ import System.Environment.FindBin
 
 data LogLevel
     = Debug | Info | Warning | Error
-  deriving Show
+  deriving (Eq, Ord, Show)
+
+printLogLevel = Info
 
 -- | Logs a message with the given log level.
 -- Prints to stdout on unix, uses a logFile on windows.
 logg :: LogLevel -> String -> IO ()
 logg ll msg =
+    when (ll >= printLogLevel) $
     inner $ mkMsg ll msg
   where
     inner = case System.Info.os of
