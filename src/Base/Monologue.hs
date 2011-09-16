ddeaba7875ba8fe416f14f0a48fb77a35652e2a2
shahn <shahn@joyridelabs.de>
2011-09-16 22:16:52 +0800

    story-mode: don't crash on missing monologues

--- src/Base/Monologue.hs
+++ src/Base/Monologue.hs
@@ -2,6 +2,10 @@
 module Base.Monologue (Monologue, readStoryModeMonologue) where
 
 
+import Prelude hiding (catch)
+
+import Control.Exception
+
 import System.FilePath
 
 import Utils
@@ -18,8 +22,11 @@ readStoryModeMonologue :: String -> IO Monologue
 readStoryModeMonologue name = do
     mFile <- getStoryModeMonologueFile name
     case mFile of
-        Just file -> parseMonologue <$> io (readFile file)
+        Just file -> catch (parseMonologue <$> io (readFile file)) errorHandler
         Nothing -> return [pv ("monologue file not found: " ++ monologueDir </> name)]
+  where
+    errorHandler :: IOException -> IO Monologue
+    errorHandler e = return [pv ("Monologue" <~> show name <~> "not found!")]
 
 getStoryModeMonologueFile :: String -> IO (Maybe FilePath)
 getStoryModeMonologueFile name = do
