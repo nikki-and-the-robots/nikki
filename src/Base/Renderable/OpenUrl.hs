cd5d5f76032abb04a30135b6706a5d8ec30fe27d
shahn <shahn@joyridelabs.de>
2011-10-04 23:45:36 +0800

    show message when opening a url in the desktop-browser

--- src/Base/Renderable/OpenUrl.hs
+++ src/Base/Renderable/OpenUrl.hs
@@ -15,10 +15,15 @@ import Base.Renderable.Message
 openUrl :: Application -> String -> Parent -> AppState
 openUrl app url parent = NoGUIAppState $ do
     success <- io $ qtOpenUrl url
-    return $ if success then parent else message app text parent
+    return $ message app (text success) parent
   where
-    text =
+    text False =
         p "Something went wrong with opening your webbrowser." :
         p "Please manually point your browser to:" :
+        pv url :
+        []
+    text True =
+        p "A browser should open up." :
+        p "If not, please manually point your browser to:" :
         pv url :
         []
