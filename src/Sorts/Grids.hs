9bc06edbf141c70086a5b3bac8aecfc35cabe015
shahn <shahn@joyridelabs.de>
2011-10-12 21:59:49 +0800

    fixed buggy rendering of horizon grids

--- src/Sorts/Grids.hs
+++ src/Sorts/Grids.hs
@@ -78,17 +78,34 @@ instance Sort GSort Grid where
         renderPixmapSimple ptr (gridPixmap sort)
     renderEditorObject ptr offset eo = do
         -- render the grid
-        resetMatrix ptr
-        translate ptr offset
         windowSize <- sizeQPainter ptr
-        let sort = editorSort eo
-            pix = gridPixmap sort
-            positions = calculateGridRenderPositions windowSize
-                            (gridType sort) (size sort) offset
+        let debugMode = False
 
-        translate ptr (Position 0 (- height (size sort)))
+            debugPadding = Position 100 100
+            usedOffset = if debugMode
+                then offset -~ debugPadding
+                else offset
+            usedWindowSize = if debugMode
+                then windowSize -~ position2size (fmap (* 2) debugPadding)
+                else windowSize
+
+            sort = editorSort eo
+            pix = gridPixmap sort
+            position = epToPosition (size sort) (eo ^. editorPosition)
+            positions = calculateGridRenderPositions usedWindowSize usedOffset
+                            (gridType sort) (size sort) position
+
+        resetMatrix ptr
+        when debugMode $ do
+            setPenColor ptr red 1
+            drawRect ptr debugPadding usedWindowSize
+
+        translate ptr offset
+
         forM_ positions $ \ p -> do
             translate ptr p
+            when debugMode $
+                fillRect ptr zero (size sort) (alpha ^= 0.5 $ yellow)
             renderPixmapSimple ptr pix
             translate ptr (negateAbelian p)
 
@@ -107,28 +124,53 @@ instance Sort GSort Grid where
 
 renderGrid :: GSort -> Offset Double -> Position Double -> Size Double -> [RenderPixmap]
 renderGrid sort offset position windowSize =
-    let pix = gridPixmap sort
-        positions = calculateGridRenderPositions windowSize
-                        (gridType sort) (size sort) (position +~ offset)
-    in (flip map positions $ \ p -> RenderPixmap pix (position +~ p) Nothing)
+    let debugMode = False
+        debugPadding = Position 100 100
+        debugCommands =
+            map (\ p ->
+                 RenderCommand zero $ \ ptr -> fillRect ptr p (size sort) (alpha ^= 0.5 $ yellow))
+                positions ++
+            (RenderCommand zero $ \ ptr ->
+                resetMatrix ptr *>
+                setPenColor ptr red 1 *>
+                drawRect ptr debugPadding usedWindowSize) :
+            []
+
+        pix = gridPixmap sort
+        usedWindowSize = if debugMode
+            then Size 500 300
+            else windowSize
+        usedOffset = if debugMode
+            then offset -~ debugPadding
+            else offset
+        positions = calculateGridRenderPositions usedWindowSize usedOffset
+                        (gridType sort) (size sort) position
+
+    in (flip map positions $ \ p -> RenderPixmap pix p Nothing) ++
+       if debugMode then debugCommands else [] ++
+       []
+
 
 
 -- | calculates where to render the grid, so that every space of the screen is filled
-calculateGridRenderPositions :: Size Double -> GridType -> Size Double
+calculateGridRenderPositions :: Size Double -> Offset Double -- ^ rendering offset
+    -> GridType -> Size Double
     -> Position Double -- ^ actual render position
     -> [Position Double]
-calculateGridRenderPositions windowSize typ tileSize position =
+calculateGridRenderPositions windowSize offset typ tileSize position =
     map toRenderPosition intPositions
   where
     toRenderPosition :: (Int, Int) -> Position Double
-    toRenderPosition (x, y) = 
+    toRenderPosition (x, y) =
+        position +~
         Position (fromIntegral x * width tileSize) (fromIntegral y * height tileSize)
     intPositions :: [(Int, Int)]
-    intPositions = case typ of
-        Tiled -> cartesian [xmin .. xmax] [ymin .. ymax]
-        WrappedHorizon -> map (\ x -> (x, 0)) [xmin .. xmax]
+    intPositions = cartesian [xmin .. xmax] ys
+    ys = case typ of
+        Tiled -> [ymin .. ymax]
+        WrappedHorizon -> if 0 >= ymin && 0 <= ymax then [0] else []
 
-    xmin = floor ((0 - positionX position) / width tileSize)
-    xmax = floor ((width windowSize - positionX position) / width tileSize)
-    ymin = floor ((0 - positionY position) / height tileSize)
-    ymax = floor ((height windowSize - positionY position) / height tileSize)
+    xmin = floor ((- positionX offset - positionX position) / width tileSize)
+    xmax = floor ((- positionX offset + width windowSize - positionX position) / width tileSize)
+    ymin = floor ((- positionY offset - positionY position) / height tileSize)
+    ymax = floor ((- positionY offset + height windowSize - positionY position) / height tileSize)
