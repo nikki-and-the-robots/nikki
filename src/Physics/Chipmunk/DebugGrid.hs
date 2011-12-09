528d80b9e91f8474fc3da577271d506a00d3fa6e
shahn <shahn@joyridelabs.de>
2011-12-09 22:07:36 +0800

    render centers of chipmunks in debug mode (-c)

--- src/Physics/Chipmunk/DebugGrid.hs
+++ src/Physics/Chipmunk/DebugGrid.hs
@@ -17,7 +17,8 @@ import Utils
 
 
 renderGrids :: Ptr QPainter -> Qt.Position Double -> [Chipmunk] -> IO ()
-renderGrids ptr offset chips =
+renderGrids ptr offset chips = do
+    setPenColor ptr signalRed 1
     mapM_ (renderGrid ptr offset) chips
 
 renderGrid :: Ptr QPainter -> Qt.Position Double -> Chipmunk -> IO ()
@@ -29,6 +30,8 @@ renderGrid ptr offset chip = do
 
     translateVector ptr position
     Qt.rotate ptr (rad2deg rad)
+    when (position /= zero) $
+        drawCircle ptr zero 5
 
     mapM_ (renderShapeType ptr) (shapeTypes chip)
 
@@ -39,11 +42,9 @@ renderShapeType ptr ShapeDescription{shapeType, shapeOffset} =
             mapM_ (uncurry (renderVectorLine ptr)) (adjacentCyclic $ map (offset +~) vertices)
         (LineSegment start end thickness, offset) ->
             renderVectorLine ptr (start +~ offset) (end +~ offset)
-        (Circle radius, vec) -> do
-            setPenColor ptr signalRed 1
+        (Circle radius, vec) ->
             drawCircle ptr (vector2position vec) (realToFrac radius)
 
 renderVectorLine :: Ptr QPainter -> Vector -> Vector -> IO ()
-renderVectorLine ptr a b = do
-    setPenColor ptr signalRed 1
+renderVectorLine ptr a b =
     drawLine ptr (vector2position a) (vector2position b)
