7fe8874b98690472e879cce5023b85876af053af
shahn <shahn@joyridelabs.de>
2011-12-14 01:53:19 +0800

    story-mode: configuration of level files

--- src/StoryMode/Configuration.hs
+++ src/StoryMode/Configuration.hs
@@ -15,13 +15,22 @@ episodes =
 
 episode_1 = Episode
     Episode_1
-    "ep1-intro-v02"
-    (
-        "stash" :
-        "stush" :
-    [])
-    "ep1-x-v03"
+    "00-intro"
+       (
+        "00-intro" :
+        "01-grotto" :
+        "02-platform-training" :
+        "03-powergrid" :
+        "04-hanoi" :
+        "05-lasertower" :
+        "06-suivez-moi" :
+        "07-lootninja" :
+        "09-tree-climbing" :
+        "10-greed" :
+        [])
+    "x"
 
 epPathSnippet Episode_1 = "ep1"
 
 epTitle Episode_1 = "Episode 1"
+
