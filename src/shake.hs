c9b3229cc9a79603a60fab55b3dfdbf8321e0e82
shahn <shahn@joyridelabs.de>
2011-12-24 23:42:11 +0800

    better shake script

--- src/shake.hs
+++ src/shake.hs
@@ -1,91 +1,140 @@
 #!/usr/bin/env runghc
 
-
 import Data.List
+import Data.Char
 
 import Control.Applicative ((<$>))
+import Control.Monad
 import Control.Monad.IO.Class
+import Control.Arrow
 
-import System.Directory
-import System.FilePath
-
-import Utils.Scripting
+import System.Directory hiding (doesFileExist)
+import System.IO
 
 import Development.Shake
+import Development.Shake.FilePath
 
 
-main = shake shakeOptions{shakeParallel = 2, shakeVerbosity = 2} $ do
-    let core = "dist" </> "build" </> "core" </> "core"
-        qtWrapper = "cpp" </> "dist" </> "libqtwrapper.a"
+-- | Returns all recursive dependencies of the given haskell file.
+-- (Recursive module imports will result in an infinite loop.)
+getHaskellDeps :: FilePath -> Action [FilePath]
+getHaskellDeps file = do
+    exists <- doesFileExist file
+    when (not exists) $
+        error ("file not found: " ++ file)
+    rec [] [file]
+  where
+    rec :: [FilePath] -> [FilePath] -> Action [FilePath]
+    rec done [] = return []
+    rec done (file : queue) = do
+        exists <- doesFileExist file
+        if not exists then rec (file : done) queue else do
+            files <- extractHaskellDeps file
+            let done' = file : done
+                queue' = nub (queue ++ filter (not . (`elem` done)) files)
+            indirectImports <- rec done' queue'
+            return (file : indirectImports)
+
+hsImports :: String -> [String]
+hsImports xs = [ takeWhile (\x -> isAlphaNum x || x `elem` "._") $ dropWhile (not . isUpper) x
+               | x <- lines xs, "import " `isPrefixOf` x]
+
+extractHaskellDeps :: FilePath -> Action [FilePath]
+extractHaskellDeps file = do
+    code <- liftIO $ readFile file
+    let imports = hsImports code
+    filterM doesFileExist $ map moduleToFile imports
+
+moduleToFile :: String -> FilePath
+moduleToFile =
+    map (\ c -> if c == '.' then '/' else c) >>>
+    (++ ".hs")
+
+
+pkgs =
+    "transformers" :
+    "clocked" :
+    "mtl" :
+    "data-accessor" :
+    "Hipmunk" :
+    "safe" :
+    "text" :
+    "bytestring" :
+    "deepseq" :
+    "binary" :
+    "binary-communicator" :
+    "aeson" :
+    "directory" :
+    "temporary" :
+    "vector" :
+    "download-curl" :
+    "strict" :
+    "filepath" :
+    "email-validate" :
+    "bifunctors" :
+    "LibZip" :
+    "RSA" :
+    "sfml-audio" :
+    "cmdargs" :
+    "data-accessor-mtl" :
+    "FindBin" :
+    "uniplate" :
+    "template" :
+    []
+
+
+main = do
+  hSetBuffering stdout NoBuffering
+  shake shakeOptions{shakeParallel = 2, shakeVerbosity = 2} $ do
+    let qtWrapper = "cpp" </> "dist" </> "libqtwrapper.a"
         cppMakefile = "cpp" </> "dist" </> "Makefile"
-        cabalConfiguration = "dist" </> "setup-config"
+        ghcFlags =
+            "-O0" :
+            "-threaded" :
+            map ("-package=" ++) pkgs
 
+    want ["core"]
 
-    want [core]
+    "core" *> \ core -> do
+        os <- map (flip replaceExtension "o") <$> getHaskellDeps "Main.hs"
+        need (qtWrapper : os)
+        let libFlags = ["-lqtwrapper", "-Lcpp/dist", "-lQtGui", "-lQtOpenGL"]
+        system' "ghc" $ ["-o",core] ++ libFlags ++ os ++ ghcFlags
 
-    core *> \ _ -> do
-        need [qtWrapper, cabalConfiguration]
-        hss <- getHaskellSources
-        need hss
-        liftIO $ print ("NYI: too much haskell files as deps")
-        system' "cabal" ["build"]
+    "//*.o" *> \ o -> do
+        let hsFile = replaceExtension o "hs"
+        need [hsFile]
+        deps <- extractHaskellDeps hsFile
+        need (map (flip replaceExtension "hi") deps)
+        system' "ghc" $ ["-c", hsFile] ++ ghcFlags
 
-    cabalConfiguration *> \ _ -> do
-        need [qtWrapper]
-        need ["nikki.cabal"]
-        system' "cabal" ["configure"]
+    "//*.hi" *> \ hi -> do
+        need [replaceExtension hi "o"]
 
     qtWrapper *> \ _ -> do
         cs <- map ("cpp" </>) <$> getDirectoryFiles "cpp" "*.cpp"
         hs <- map ("cpp" </>) <$> getDirectoryFiles "cpp" "*.h"
         need (cppMakefile : cs ++ hs)
-        withDirectory ("cpp" </> "dist") $ do
-            system' "make" []
+        system' "bash" ("-c" : "cd cpp/dist; make" : [])
 
     cppMakefile *> \ _ -> do
         need ["cpp" </> "CMakeLists.txt"]
         liftIO $ createDirectoryIfMissing False ("cpp" </> "dist")
-        withDirectory ("cpp" </> "dist") $ do
-            system' "cmake" [".."]
-
-getHaskellSources :: Action [FilePath]
-getHaskellSources =
-    filter (".hs" `isSuffixOf`) <$> (liftIO $ getFilesRecursive ".")
-
-
-haskellSuperModules =
-    "Base" :
-    "Control" :
-    "Data" :
-    "Distribution" :
-    "Editor" :
-    "FakeFutureVersion" :
-    "Game" :
-    "Graphics" :
-    "Legacy" :
-    "LevelServer" :
-    "Main" :
-    "Network" :
-    "Object" :
-    "Physics" :
-    "Profiling" :
-    "Setup" :
-    "Sorts" :
-    "StoryMode" :
-    "System" :
-    "Text" :
-    "Top" :
-    "Utils" :
-    "Version" :
-    []
+        system' "bash" ("-c" : "cd cpp/dist; cmake .." : [])
 
 
 -- * utils
 
-withDirectory :: MonadIO m => FilePath -> m a -> m a
-withDirectory dir action = do
+_withDirectory :: MonadIO m => FilePath -> m a -> m a
+_withDirectory dir action = do
     outer <- liftIO getCurrentDirectory
     liftIO $ setCurrentDirectory dir
     r <- action
     liftIO $ setCurrentDirectory outer
     return r
+
+objectOrInterfaceFile :: FilePath -> Bool
+objectOrInterfaceFile f =
+    ext == ".hi" || ext == ".o"
+  where
+    ext = takeExtension f
