49e8b8f6093f36f060b65139c546f193cc7dcb3c
cptfuro <cptfuro@joyridelabs.de>
2012-01-05 22:12:38 +0800

    StoryMode.hs updated

--- src/Sorts/StoryMode.hs
+++ src/Sorts/StoryMode.hs
@@ -105,7 +105,7 @@ tiles =
     ("tiles/neon/pac", Position 1 1, Size 716 468, 0.3, Just [0, 1, 2, 3]) :
     ("tiles/neon/tetris-01", Position 1 1, Size 332 340, 0.4, Just [0, 1, 2, 3, 4, 5]) :
     ("tiles/neon/snake", Position 1 1, Size 844 584, 0.25, Just [0, 1, 2, 0, 1, 2, 3, 4, 5]) :
-    ("tiles/neon/skull", Position 1 1, Size 588 584, 0.35, Just [0, 1, 2, 0, 1, 2, 3, 4, 5, 6, 7, 0, 7, 0, 7, 0, 7, 0]) :
+    ("tiles/neon/skull", Position 1 1, Size 588 584, 0.35, Just [0, 1, 2, 3, 4, 5, 6, 7, 0, 7, 0, 7, 0, 7, 0]) :
     ("tiles/neon/beam-standard", Position 13 13, Size 52 52, 0.25, Just [0, 1]) :
 -- temporary tiles (dummy)
     ("tiles/dummy/night-dusk", Position 0 0, Size 1920 1080, 1, Nothing) :
