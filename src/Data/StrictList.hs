fba294f2861b45ac698ddc064d67e20729bb9b79
shahn <shahn@joyridelabs.de>
2012-02-12 03:11:08 +0800

    refactoring: Data.StrictList.(:$) to (:!)

--- src/Data/StrictList.hs
+++ src/Data/StrictList.hs
@@ -8,15 +8,15 @@ import Data.Foldable as F
 
 
 data StrictList a
-    = !a :$ !(StrictList a)
+    = !a :! !(StrictList a)
     | Empty
   deriving (Show, Functor, Foldable)
 
-infixr 5 :$
+infixr 5 :!
 
 instance Monoid (StrictList a) where
     mempty = Empty
-    mappend (a :$ r) bs = a :$ mappend r bs
+    mappend (a :! r) bs = a :! mappend r bs
     mappend Empty bs = bs
 
 instance Monad StrictList where
@@ -28,29 +28,29 @@ instance Monad StrictList where
 type SL a = StrictList a
 
 fromList :: [a] -> StrictList a
-fromList (a : r) = a :$ fromList r
+fromList (a : r) = a :! fromList r
 fromList [] = Empty
 
 singleton :: a -> StrictList a
-singleton = (:$ Empty)
+singleton = (:! Empty)
 
 (!) :: StrictList a -> Int -> a
-(a :$ r) ! 0 = a
-(_ :$ r) ! i = r ! pred i
+(a :! r) ! 0 = a
+(_ :! r) ! i = r ! pred i
 Empty ! _ = error "(!): index out of bounds"
 
 slFilter :: (a -> Bool) -> StrictList a -> StrictList a
-slFilter p (a :$ r) =
-    (if p a then (a :$) else id) (slFilter p r)
+slFilter p (a :! r) =
+    (if p a then (a :!) else id) (slFilter p r)
 slFilter _ Empty = Empty
 
 filterM :: Monad m => (a -> m Bool) -> StrictList a -> m (StrictList a)
-filterM pred (a :$ r) = do
+filterM pred (a :! r) = do
     c <- pred a
     r' <- filterM pred r
     if c then do
         r' <- filterM pred r
-        return (a :$ r')
+        return (a :! r')
       else do
         r' <- filterM pred r
         return r'
@@ -63,16 +63,16 @@ filterM pred Empty = return Empty
 -- do also. (This means, the element will not be inserted,
 -- if it's already in the list.)
 insertUnique :: Ord a => a -> SL a -> SL a
-insertUnique a (b :$ r) =
+insertUnique a (b :! r) =
     case compare a b of
-        GT -> b :$ insertUnique a r
-        EQ -> b :$ r -- not inserted
-        LT -> a :$ b :$ r
+        GT -> b :! insertUnique a r
+        EQ -> b :! r -- not inserted
+        LT -> a :! b :! r
 insertUnique a Empty = singleton a
 
 delete :: Eq a => a -> SL a -> SL a
-delete x (a :$ r) =
+delete x (a :! r) =
     if x == a
     then r
-    else a :$ delete x r
+    else a :! delete x r
 delete x Empty = Empty
