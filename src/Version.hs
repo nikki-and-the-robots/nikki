9df13c15b7cd6df430f3c39ab76001992cfed5c8
shahn <shahn@joyridelabs.de>
2012-08-06 22:18:55 +0800

    version bump to 0.5.1.2

--- src/Version.hs
+++ src/Version.hs
@@ -10,5 +10,5 @@ import Data.Version
 nikkiVersion :: Version
 nikkiVersion = Version version tags
   where
-    version = 0 : 5 : 1 : 1 : []
+    version = 0 : 5 : 1 : 2 : []
     tags = ["samuel"]
