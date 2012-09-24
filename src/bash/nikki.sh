d2aabbaf95e9fc4c7ba4ace9229466566afdf698
iwan <iwan@joyridelabs.de>
2012-09-24 22:48:24 +0800

    allowed nikki to run from path containing spaces

--- src/bash/nikki.sh
+++ src/bash/nikki.sh
@@ -8,7 +8,7 @@ exit 1
 trap error ERR
 
 # setting LD_LIBRARY_PATH
-deployDir=$(dirname $(readlink -f $0))
+deployDir=$(dirname "$(readlink -f $0)")
 export LD_LIBRARY_PATH=$deployDir:$LD_LIBRARY_PATH
 
-$deployDir/restarter $@
+"$deployDir/restarter" $@
